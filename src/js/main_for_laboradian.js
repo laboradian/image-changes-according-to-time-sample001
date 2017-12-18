d48083d190e284120134cfedb94e1b1ff9e27833
kura5 <laboradian@gmail.com>
2017-12-18 13:45:50 +0900

    目的の要素がない場合は処理をしないようにした
    別のヘッダデザインを使ったページを新たに使用するため
    (laboradian.com)

--- src/js/main_for_laboradian.js
+++ src/js/main_for_laboradian.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
   const headerImageBkId = 'lr-header-image-bg';
   //const headerImageBkId = 'middleLayer';
   const headerImageBk = document.querySelector(`#${headerImageBkId}`);
+  if (!headerImageBk) return;
   const bgCreator = new BackgroundCreator();
   bgCreator.update();
   bgCreator.translateToCurrentBackground(headerImageBk, 3);
